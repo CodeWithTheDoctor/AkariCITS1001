677b9fe7fb72037cfe000b5c899feb052958e72f
Ash <ashishithape.ai@gmail.com>
2021-05-17 11:20:17 +0800

    Almost completed display puzzle method.

--- AkariViewer.java
+++ AkariViewer.java
@@ -6,12 +6,13 @@
  */
 import java.awt.*;
 import java.awt.event.*; 
+import java.util.Arrays;
 
 public class AkariViewer implements MouseListener
 {    
     private Akari puzzle;    // the internal representation of the puzzle
     private SimpleCanvas sc; // the display window
-
+    private boolean[][] isLit;
     /**
      * Constructor for objects of class AkariViewer.
      * Sets all fields and displays the initial puzzle.
@@ -36,7 +37,7 @@ public class AkariViewer implements MouseListener
         this(new Akari("Puzzles/p" + n / 10 + "-e" + n % 10 + ".txt"));
     }
     
-    /**
+    /** 
      * Uses the provided example file on the LMS page.
      */
     public AkariViewer()
@@ -68,18 +69,26 @@ public class AkariViewer implements MouseListener
     private void displayPuzzle()
     {
         // TODO 11 - In Progress (not sure how to account for lit squares)
-        /*
+        
         Color c;
+        Space[] blackBlocks = new Space[] {Space.BLACK,Space.ZERO,Space.ONE,Space.TWO,Space.THREE,Space.FOUR};
         for (int i = 0; i < puzzle.getSize(); i++) {
             for (int j = 0; j < puzzle.getSize(); j++) {
-                if (puzzle.getBoard(i, j) == Space.EMPTY) {c = Color.white;}
-                else if (puzzle.getBoard(i, j) == Space.BLACK || ) {
-                    c = Color.black;
+                if (puzzle.getBoard(i, j) == Space.EMPTY) {
+                    if(puzzle.getLitGrid(i, j)){
+                        c = Color.YELLOW;
+                    }
+                    else {
+                        c = Color.WHITE;
+                    }
+                }
+                else if (Arrays.stream(b).anyMatch(x -> x == puzzle.getBoard(i, j))) {
+                    c = Color.BLACK;
                     
                 }
+                sc.drawRectangle(j * puzzle.getSize(). i * puzzle.getSize(), (j+1) * puzzle.getSize(), (i + 1) * puzzle.getSize(), c);
             }
         }
-        */
     }
     
     /**
